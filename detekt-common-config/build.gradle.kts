29a36149a7ece7eec1bf957514e1bd617b23bee7
alpeshvas <alpeshvash@gmail.com>
2024-10-31 13:31:25 +0530

    remove things

--- detekt-common-config/build.gradle.kts
+++ detekt-common-config/build.gradle.kts
@@ -10,7 +10,6 @@ repositories {
 
 dependencies {
     compileOnly("io.gitlab.arturbosch.detekt:detekt-api:1.23.7")
-//    detektPlugins(project(":detekt-common-config"))
     testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.20.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
