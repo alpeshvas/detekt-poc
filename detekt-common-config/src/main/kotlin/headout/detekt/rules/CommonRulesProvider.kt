ce385afda30fc2a3f67280a8f46c68ef8671175b
alpeshvas <alpeshvash@gmail.com>
2024-10-31 13:36:06 +0530

    rule set change doesn't work

--- detekt-common-config/src/main/kotlin/headout/detekt/rules/CommonRulesProvider.kt
+++ detekt-common-config/src/main/kotlin/headout/detekt/rules/CommonRulesProvider.kt
@@ -5,7 +5,7 @@ import io.gitlab.arturbosch.detekt.api.RuleSet
 import io.gitlab.arturbosch.detekt.api.RuleSetProvider
 
 open class CommonRulesProvider : RuleSetProvider {
-    override val ruleSetId: String = "headout-common-detekt-rules"
+    override val ruleSetId: String = "abcdef"
 
     override fun instance(config: Config): RuleSet {
         return RuleSet(
